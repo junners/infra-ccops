03d16de7a078f7af061b157e77dc9be91251e22b
junners <jose.junryan@gmail.com>
2025-06-28 16:54:36 -0700

    feat: add project service before injecting

--- src/core/service/project-service.ts
+++ src/core/service/project-service.ts
@@ -1,4 +1,46 @@
 import { injectable } from 'inversify';
+import { existsSync, readFileSync } from 'node:fs';
+import { cwd } from 'node:process';
+import { parse } from 'yaml';
+
+export type ProjectConfiguration = {
+  name: string;
+};
+
+const SERVICE_CONFIGURATION: string = 'config/service.yml';
+const CONFIGURATION_FILE_DOES_NOT_EXIST: string = "Service configuration file doesn't exist";
+const BAD_USAGE_GET_CONFIG: string = 'Bad usage of getConfig(), use resolve() prior to calling getConfig()';
 
 @injectable()
-export class ProjectService {}
+export class ProjectService {
+  private configuration!: ProjectConfiguration;
+
+  public getConfig(): ProjectConfiguration {
+    if (!this.configuration) {
+      throw new Error(BAD_USAGE_GET_CONFIG);
+    }
+
+    return this.configuration;
+  }
+
+  public async resolve(projectDirectory?: string): never | Promise<void> {
+    const directoryToRead = projectDirectory ?? cwd();
+    const validatedFile = this.validate(directoryToRead);
+    this.configuration = this.readConfiguration<ProjectConfiguration>(validatedFile);
+  }
+
+  private readConfiguration<T>(configFile: string): T {
+    const file = readFileSync(configFile, 'utf8');
+    return parse(file) as T;
+  }
+
+  private validate(projectDirectory: string): never | string {
+    const configFilePath = `${projectDirectory}/${SERVICE_CONFIGURATION}`;
+    const configExist = existsSync(configFilePath);
+    if (!configExist) {
+      throw new Error(CONFIGURATION_FILE_DOES_NOT_EXIST);
+    }
+
+    return configFilePath;
+  }
+}
--- test/core/service/project-service.test.ts
+++ test/core/service/project-service.test.ts
@@ -1,10 +1,43 @@
+import esmock from 'esmock';
 import { expect } from 'expect';
-import { describe, it } from 'node:test';
+import { beforeEach, describe, it } from 'node:test';
+import { stub } from 'sinon';
 
-import { ProjectService } from '../../../src/core/service/project-service.ts';
+import { ProjectService } from '../../../src/core/service/project-service.js';
 
 describe('ProjectService', () => {
+  let sut: ProjectService;
+
+  beforeEach(() => {
+    sut = new ProjectService();
+  });
+
   it('should be defined', () => {
     expect(ProjectService).toBeDefined();
+  });
+
+  describe('resolve', () => {
+    it('should validate that config/service.yml exist', async () => {
+      const { ProjectService: Service } = await esmock('../../../src/core/service/project-service.js', {
+        'node:fs': {
+          existsSync: stub().returns(false),
+          readFileSync: stub().returns('name: automation'),
+        },
+      });
+      sut = new Service();
+      await expect(sut.resolve('/project/directory')).rejects.toThrow();
+    });
+
+    it('should read config/service.yml', async () => {
+      const { ProjectService: Service } = await esmock('../../../src/core/service/project-service.js', {
+        'node:fs': {
+          existsSync: stub().returns(true),
+          readFileSync: stub().returns('name: automation'),
+        },
+      });
+      sut = new Service();
+      await expect(sut.resolve('/project/directory')).resolves.not.toThrow();
+      expect(sut.getConfig()).toStrictEqual({ name: 'automation' });
+    });
   });
 });
